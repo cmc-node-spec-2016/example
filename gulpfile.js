c8b6e42ca0eb93689dc9c9b469c89c3f8c605fc7
Сковорода Никита Андреевич <chalkerx@gmail.com>
2016-03-17 16:36:02 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+'use strict';
+
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var plumber = require('gulp-plumber');
+var babel = require('gulp-babel');
+
+var files = {
+    libs: 'libs/**/*.js'
+};
+
+var babelOptions = {
+    presets: ['es2015-node5', 'stage-3']
+};
+
+gulp.task('libs', function () {
+    gulp.src(files.libs)
+        .pipe(plumber())
+        .pipe(changed('build'))
+        .pipe(babel(babelOptions))
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['libs']);
+
+gulp.task('default', ['build'], function() {
+    gulp.watch(files.libs, ['libs']);
+});
---
+++ libs/test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function times2(x) {
+  return new Promise((accept, reject) => {
+    setTimeout(() => {
+      accept(x * 2);
+    }, 100);
+  });
+}
+
+async function times4(x) {
+  x = await times2(x);
+  x = await times2(x);
+  return x;
+}
+
+const foo = async function() {
+  let x = 2;
+  x = await times4(x);
+  console.log(x);
+};
+
+foo().catch((err) => {
+  console.log('Error: ' + err);
+});
